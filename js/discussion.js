93b54490a182d0fc4cda2a49562258e0f5a76d16
yemoujian <857873586@qq.com>
2022-12-17 19:44:31 +0800

    update

--- js/discussion.js
+++ js/discussion.js
@@ -2,4 +2,4 @@ $(document).ready(function (){
     (function fun2() { $("#pinlun").on("click", "button",
     function() { $(this).parent().parent().remove(); })})()
 })
-function fun1(){ $('#pinlun').append( "<div id='neirong'>" + text.value+"<br><p><button id='btn_2'>删除评论</button></p></div>"); text.value="";}
+function fun1(){ $('#pinlun').append( "<div id='neirong'>" + text.value+"<br><p><button id='btn_2'>×</button></p></div>"); text.value="";}
